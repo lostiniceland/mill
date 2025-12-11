33af260da11636838a798f3c2a0a92698772d3a4
Marc Schlegel <marc.schlegel@gmx.de>
2025-12-11 16:53:58 +0100

    pfusch

--- example/groovylib/basic/6-programmable/foo/src/foo/Foo.kt
+++ example/groovylib/basic/6-programmable/foo/src/foo/Foo.kt
@@ -1,14 +1,9 @@
 package foo
 
-import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.required
-import kotlinx.html.h1
-import kotlinx.html.stream.createHTML
-
+import groovy.cli.commons.CliBuilder
+import groovy.xml.MarkupBuilder
 class Foo : CliktCommand() {
     val text by option("-t", "--text", help = "text to insert").required()
-
     override fun run() {
         echo(generateHtml(text))
     }
